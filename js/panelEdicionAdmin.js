23e0866bb7c16c811ac4253e2c6a77d3fb14de44
Victor Martinez <victor.martinez.zamora98@gmail.com>
2021-08-11 17:26:52 -0500

    Formulario validación admin

--- js/panelEdicionAdmin.js
+++ js/panelEdicionAdmin.js
@@ -10,8 +10,10 @@ function validateForm(e){//validateForm
     //No ejecutes
     e.preventDefault();
     let inputProduct = document.getElementById('product');
-    let inputPrice = document.getElementById('price');
-    let inputNotes = document.getElementById('notas');
+    let inputMarca = document.getElementById('marca');
+    let inputModel = document.getElementById('model');
+    let inputDescription = document.getElementById('description');
+    let inputImage = document.getElementById('image');
 
     console.log(typeof(inputProduct.value))
     //let inputZipcode = document.getElementById('zipCode')
@@ -19,12 +21,12 @@ function validateForm(e){//validateForm
     //console.log(inputNombre.value)
     console.log(inputProduct.classList)
 
-    function validateName(nombre){
+    function validateName(nombre){//validateName
       //let expReg= /^[A-Z]+$/;
-      let expReg = new RegExp(/^[a-zA-Z ]+$/)
-      let esValido2 = expReg.test(nombre);
-      console.log(esValido2);
-      if(esValido2 == true){//if nombre
+      let expReg = new RegExp(/^[-a-zA-Z-á-ú-0-9. ]+$/)  
+      let esValido = expReg.test(nombre);
+      console.log(esValido);
+      if(esValido == true){//if nombre
           inputProduct.classList.remove('is-invalid')
           inputProduct.classList.add('is-valid')
           console.log('Hey si es mayor a =0')
@@ -37,26 +39,96 @@ function validateForm(e){//validateForm
     
     validateName(inputProduct.value)
     
-    if(inputPrice.value.length > 0){//if teléfono
-        
-            inputPrice.classList.remove('is-invalid')
-            inputPrice.classList.add('is-valid') 
-            valid++;
-    }else{
-      inputPrice.classList.add('is-invalid')
-    }//if teléfono
+    
+    function validateMarca(marca){//validateMarca
+      //let expReg= /^[A-Z]+$/;
+      let expReg = new RegExp(/^[-a-zA-Z-á-ú-0-9. ]+$/)
+      let esValido2 = expReg.test(marca);
+      console.log(esValido2);
+      if(esValido2 == true){//if Marca
+          inputMarca.classList.remove('is-invalid')
+          inputMarca.classList.add('is-valid')
+          console.log('Hey si es mayor a =0')
+          return valid ++;
+        }else{
+          inputMarca.classList.add('is-invalid')
+          console.log('Esta vacío intenta nuevamente');
+      }//if Marca
+    }//validateMarca
+    
+    validateMarca(inputMarca.value)
 
-    if(inputNotes.value.length != 0 || inputPrice.value.length === 0){//if opcional
-      inputNotes.classList.remove('is-invalid')
-      inputNotes.classList.add('is-valid') 
-      valid++;
-    }else{
-      inputNotes.classList.add('is-valid')
-      valid++;
-    }//if opcional
+    function validateModel(model){//validateModel
+      //let expReg= /^[A-Z]+$/;
+      let expReg = new RegExp(/^[-a-zA-Z-á-ú-0-9. ]+$/)
+      let esValido3 = expReg.test(model);
+      console.log(esValido3);
+      if(esValido3 == true){//if Model
+          inputModel.classList.remove('is-invalid')
+          inputModel.classList.add('is-valid')
+          console.log('Hey si es mayor a =0')
+          return valid ++;
+        }else{
+          inputModel.classList.add('is-invalid')
+          console.log('Esta vacío intenta nuevamente');
+      }//if Model
+    }//validateModel
+    
+    validateModel(inputModel.value)
+    
+    // if(inputPrice.value.length > 0){//if price
+        
+    //         inputPrice.classList.remove('is-invalid')
+    //         inputPrice.classList.add('is-valid') 
+    //         valid++;
+    // }else{
+    //   inputPrice.classList.add('is-invalid')
+    // }//if price
+
+
+    // function ValidarTamaño(obj){
+    //   var uploadFile = obj.files[0];
+     
+    //   var img = new Image();
+    //   img.onload = function (){
+    //       if (this.width.toFixed(0) != 413 && this.height.toFixed(0) != 531){
+    //             alert("La imagen debe ser de tamaño 413px por 531px.");
+    //             $('#foto').val("");
+    //       }
+          
+    //       };
+    //   img.src = URL.createObjectURL(uploadFile);
+    //   }
+
+    // function validateImage(image){
+    //   let uploadFile = image.files[0];
+    //   let img = new Imagen();
+    //   img.onload
+    // }//ValidateImage
+
+
+
+
+    function validateDescription(description){//validateDescription
+      //let expReg= /^[A-Z]+$/;
+      let expReg = new RegExp(/^[-a-zA-Z-á-ú-0-9.| ]+$/)
+      let esValido4 = expReg.test(description);
+      console.log(esValido4);
+      if(esValido4 == true){//if Description
+          inputDescription.classList.remove('is-invalid')
+          inputDescription.classList.add('is-valid')
+          console.log('Hey si es mayor a =0')
+          return valid ++;
+        }else{
+          inputDescription.classList.add('is-invalid')
+          console.log('Esta vacío intenta nuevamente');
+      }//if Description
+    }//validateDescription
+
+    validateDescription(inputDescription.value)
 
     if (valid==4){
-      window.open('mailto:generation.c2.cdmxe3@gmail.com?subject=Contacto&body=body');
+      console.log('Done');
     }
     return valid = 0;
 }//validateForm
