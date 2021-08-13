0342c5317e38446cb56dd4846e485051fbe2add2
carlos <carlos.dan.pvst@gmail.com>
2021-08-12 19:13:44 -0500

    Primera iteración para los formularios de InicioSesion y Registro

--- js/iniciarSesion.js
+++ js/iniciarSesion.js
@@ -10,54 +10,9 @@ let valid = 0;
 function validateForm(e){//validateForm
     //No ejecutes
     e.preventDefault();
-    let inputNombre = document.getElementById('name');
-    let inputApellidos = document.getElementById('lastName');
-    let inputEmail = document.getElementById('email');
-    let inputPhone = document.getElementById('phone');
-    let inputContraseña = document.getElementById('password');
-    let inputConfirmarContraseña = document.getElementById('confirmPassword');
+    let inputEmail = document.getElementById('emailIniciar');
+    let inputContraseña = document.getElementById('passwordIniciar');
 
-    console.log(typeof(inputNombre.value))
-    //let inputZipcode = document.getElementById('zipCode')
-    
-    //console.log(inputNombre.value)
-    console.log(inputNombre.classList)
-
-    function validateName(nombre){
-      //let expReg= /^[A-Z]+$/;
-      let expReg = new RegExp(/^[a-zA-Z-á-ú ]+$/)  
-      let esValido2 = expReg.test(nombre);
-      console.log(esValido2);
-      if(esValido2 == true){//if nombre
-          inputNombre.classList.remove('is-invalid')
-          inputNombre.classList.add('is-valid')
-          console.log('Hey si es mayor a =0')
-          return valid ++;
-        }else{
-          inputNombre.classList.add('is-invalid')
-          console.log('Esta vacío intenta nuevamente');
-      }//if nombre
-    }//validateName
-    
-    validateName(inputNombre.value)
-
-    function validateLastName(apellidos){
-        //let expReg= /^[A-Z]+$/;
-        let expReg = new RegExp(/^[a-zA-Z-á-ú ]+$/)  
-        let esValido2 = expReg.test(apellidos);
-        console.log(esValido2);
-        if(esValido2 == true){//if apellido
-            inputApellidos.classList.remove('is-invalid')
-            inputApellidos.classList.add('is-valid')
-            console.log('Hey si es mayor a =0')
-            return valid ++;
-          }else{
-            inputApellidos.classList.add('is-invalid')
-            console.log('Esta vacío intenta nuevamente');
-        }//if apellidos
-      }//validateLastName
-      
-      validateLastName(inputApellidos.value)
 
     function validateEmail(email){//validateEmail
       let expReg = new RegExp(/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/);
@@ -93,32 +48,13 @@ function validateForm(e){//validateForm
 
     validatePassword(inputContraseña.value)
 
-    if(inputConfirmarContraseña.value == inputContraseña.value){//ifConfirmarContraseña
-      inputConfirmarContraseña.classList.remove('is-invalid');
-      inputConfirmarContraseña.classList.add('is-valid');
-      valid++;
-    }else{
-      inputConfirmarContraseña.classList.add('is-invalid')
-    }//if ConfirmarContraseña
-
-    
-    if(inputPhone.value.length === 10 || inputPhone.value.length === 12){//if teléfono
-        
-            inputPhone.classList.remove('is-invalid')
-            inputPhone.classList.add('is-valid') 
-            valid++;
-    }else{
-      inputPhone.classList.add('is-invalid')
-    }//if teléfono
-
-
     if (valid==6){
       window.open('mailto:generation.c2.cdmxe3@gmail.com?subject=Contacto&body=body');
+      window.localStorage("sesionesRegistradas",JSON.stringify())
     }
-    return valid = 0;
+    return vaid = 0;
 }//validateForm
 
 //Eventos
 
 form.addEventListener('submit',validateForm)
-
